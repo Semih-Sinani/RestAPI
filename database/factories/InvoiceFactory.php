c73665cce2ed7010ccddeeb15768748940e8fe6b
Semih-Sinani <semihsinaniofficial@gmail.com>
2024-02-29 19:51:08 +0100

    Designed and Seeded the Database of Laravel Application

--- database/factories/InvoiceFactory.php
+++ database/factories/InvoiceFactory.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace Database\Factories;
+use App\Models\Invoice;
+use App\Models\Customer;
+
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -16,8 +19,13 @@ class InvoiceFactory extends Factory
      */
     public function definition(): array
     {
+        $status = $this->faker->randomElement(['B' , 'P' , 'V']);
         return [
-            //
+            'customer_id' => Customer::factory(),
+            'amount' => $this->faker->numberBetween(100 , 20000),
+            'status' => $status,
+            'billed_date' => $this->faker->dateTimeThisDecade(),
+            'paid_date' => $status == 'P' ? $this->faker->dateTimeThisDecade() : NULL
         ];
     }
 }
